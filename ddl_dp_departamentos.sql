558760f867cb7d464c8f64c32251e40220290598
LuisOchoa <42757672+lochoabu01@users.noreply.github.com>
2025-05-16 21:01:06 -0500

    Add files via upload

---
+++ ddl_dp_departamentos.sql
@@ -0,0 +1,7 @@
+CREATE TABLE DataLake.dbo.dp_departamentos (
+	COD_DPTO tinyint NULL,
+	NOM_DPTO nvarchar(60) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	LATITUD float NULL,
+	LONGITUD float NULL,
+	[Geo Departamento] varchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+);
